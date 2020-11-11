089eef61bd29077ef8bfac7711c4a5ef18bed411
Stjepan Hadjic <d4be4st@gmail.com>
2020-11-11 12:20:49 +0100

    Fixes

--- lib/replay_nginx_logs/request.ex
+++ lib/replay_nginx_logs/request.ex
@@ -7,8 +7,8 @@ defmodule ReplayNginxLogs.Request do
   plug Tesla.Middleware.Headers, [
     {"Content-Type", "application/vnd.api+json"},
     {"Accept", "application/vnd.api+json"},
+    {"Cookie", "_underline_session=jKEtur%2FcurjLaxqrg61pWU9hcZ3%2B%2FrA4Fm35BCjjepMkVr0JCXEpEEkx6cQ%2Fq%2F6E8MFYYY2lG%2Bmtup7C5xGfLyFgllpAlhiQf0CA1tXmWHSG6t90eoW2Gm%2BUkBPrEPej7iPuGs4T5Y26VzQhyj7bD%2BhZyz4yz%2BOL4Tos%2B5N8vZXYyIh6bAusYvrPhyAgFpQB%2Biu7t96vHU7%2BV4gK26d7BTFQz27KlSzh34l1HRmvSE%2FFAsD3uu4G3wcZPcQDhuSZAqz02LOPPHdrjHZmC7fcmwokzG6hT8vbwLeyJQHQ1CNeiwdWOwV1AEQs3GlvAwF%2Fy%2FeKORmtQlscqk%2BzaqKhGxHj7EwJ2Q%3D%3D--tlEo%2BI%2BcL9UtbvDI--TGFQIBsl%2FF%2FTxrYZQNBnIA%3D%3D"}
     # {"Cookie", "_underline_development_session=mIfidI8R0Y7rOnChDVbW2uDMFTB0qcS4AEqReXjAXIjq%2F6JUKsSAyNK5ZI7wWBrjFA%2Bbj1zZIVWK5QSv2DmHKyP9vWs3Mib78YHjx%2FVkjp19V5LOLEwEkmU%2Bx3pQpyoGaEbozA78EAC%2B7SOcsZbrmbjw%2FCdqCQs5np7rKwFU69Gk%2F1fDUOXjZMP5e5ReKzPyZXhcRMTSWB9fznyYErInuImXAzKPLcZaF9RxXpVqlcsBbQZMVhtfN%2FjEY5OVL4gT2PObkfTBkMnbDXV9HD2dgd74teDixtmZhXDoOHmuKdfGy7iSMPfRetJ50hA4Nz47O%2BW2AW229giRu7VGfcgeHBTLCdyRGCxHKam17ww0Gvp3FdhF%2FsSt65miDTWKp4yX4Nx%2FLsm5YtVCL7EqwkoCp0YxriXZa0axX6blApvQnLV9%2FGF%2BKdboGwR8R1JCgAllo1ql43tMbaB4IVnIeq8j5OLTYpU1MOF8VAYhr6SvFYjHAS9ghcyg0APWfO5V0%2FUr2bSxZ6CrmVqetKmuw5ZssrcaJ3Q0TEHIdRUiKLifSRwBCg%3D%3D--NO5oTeZWJa5ojw4s--1LS7nyJc8FaNIlbMs3dmPg%3D%3D"}
-    {"Cookie", "_underline_session=BiLYQCFQ9U%2F9GIgQosL5vIlUe3Y0hIRazT0X9%2B5%2FyEPLhlHpcz6ZQJDTvpzFGD7sx7sZOpG9uQp3ZQQBsbUXWN9cpRWzhuI6CBSaVxJegxvcArpwHUNy9asmYZ9SX9AR3qQj%2B%2FyH%2BcOKvqVM%2BD3cqdY8zXyNiaoTESIioxGEzU5DYvkZCJQr72eq--ONE4qGtwBQMo1V6w--QhM84hFsLG92ppqPNlSbOw%3D%3D; domain=.underline.io; path=/; secure; HttpOnly; SameSite=None"}
   ]
   # plug Tesla.Middleware.Logger, log_level: :debug
   # plug Tesla.Middleware.JSON
@@ -32,6 +32,6 @@ defmodule ReplayNginxLogs.Request do
     state = GenServer.call(ReplayNginxLogs.Data, :state)
     active_stream = !(state[:lectures][path] || false)
     GenServer.cast(ReplayNginxLogs.Data, {:lectures, path, active_stream})
-    patch(path, "{\"data\": {\"attributes\": {\"lectures\": {\"active_stream\": #{active_stream}}}}}")
+    patch(path, "{\"data\": {\"type\": \"lectures\", \"attributes\": {\"active_stream\": #{active_stream}}}}")
   end
 end
